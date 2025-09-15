4ddc68c105c21571fa8f11bd4da65318da2cb1c0
서유덕 <awesome36@daum.net>
2025-09-15 18:29:16 +0900

    Fix : index.ts 에 export default 로 내보내기로 수정 작업

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
-export { Calendar } from './compoentns/Calendar/Calendar';
+import { Calendar } from './components/Calendar/Calendar';
 
+// 2. 타입 export
 export type { CalendarProps } from './types/calendar';
+
+// 3. named + default export
+export { Calendar }; // named export 유지
+export default Calendar;
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,25 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), vanillaExtractPlugin()],
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, 'src/index.ts'), // 배포 진입점
+            name: 'ReactSimpleCalendar', // UMD 빌드 시 전역 이름
+            fileName: (format) => `index.${format}.js`, // 출력 파일 이름
+        },
+        rollupOptions: {
+            external: ['react', 'react-dom'], // peerDependencies 제외
+            output: {
+                globals: {
+                    react: 'React',
+                    'react-dom': 'ReactDOM',
+                },
+            },
+        },
+        sourcemap: true, // 선택 사항: 디버깅용
+    },
 });
