9008b6c3d275fe8493e7d00b83eeace773ae50ca
서유덕 <awesome36@daum.net>
2025-09-16 00:48:31 +0900

    Fix : 불필요한 폴더 삭제 및 src/index.ts 에 필요한 컴포넌트 내보내기

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 export { default as Calendar } from './components/Calendar/Calendar';
+export { default as CalendarContent } from './components/Calendar/CalendarContent';
+export { default as CalendarNav } from './components/Calendar/CalendarNav/CalendarNav';
+export { default as DayOfWeek } from './components/Calendar/DayOfWeek/DayOfWeek';
+export { default as DateGrid } from './components/Calendar/DateGrid/DateGrid';
+export { default as DateCell } from './components/Calendar/DateCell/DateCell';
 
 export type * from './types/index';
+export * from './constants/index';
--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import dts from 'vite-plugin-dts';
+import * as path from 'path';
 
 export default defineConfig({
     plugins: [react(), vanillaExtractPlugin(), dts()],
     build: {
         lib: {
-            entry: 'src/index.ts',
+            entry: path.resolve(__dirname, 'src/index.ts'),
+
             name: 'ReactSimpleCalendar', // UMD 빌드 시 전역 이름
             formats: ['es', 'umd', 'cjs'],
             fileName: (format) => `index.${format}.js`, // 출력 파일 이름
