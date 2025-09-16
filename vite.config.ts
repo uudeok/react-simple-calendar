d2780dcccf3d1ea305adca1ba9d4baf96b5a9d81
서유덕 <awesome36@daum.net>
2025-09-16 14:33:45 +0900

    Fix : package.json 및 viteconfig 내보내기 경로 수정

--- vite.config.ts
+++ vite.config.ts
@@ -9,15 +9,21 @@ export default defineConfig({
         react(),
         vanillaExtractPlugin(),
         dts({
-            insertTypesEntry: true,
-            tsconfigPath: 'tsconfig.node.json',
+            insertTypesEntry: true, // 타입 정의 진입점을 생성합니다.
+            rollupTypes: true, // 모든 타입을 단일 d.ts 파일로 묶습니다.
+            outDir: 'dist', // 출력 디렉토리를 지정합니다.
         }),
     ],
     build: {
         lib: {
             entry: path.resolve(__dirname, './src/index.ts'),
             name: 'ReactSimpleCalendar', // UMD 빌드 시 전역 이름
-            fileName: (format) => `index.${format}.js`, // 출력 파일 이름
+            fileName: (format) => {
+                if (format === 'es') return 'index.js';
+                if (format === 'umd') return 'index.umd.cjs'; // 확장자를 .cjs로 변경
+                return `index.${format}.js`;
+            },
+            formats: ['es', 'umd'], // 포맷을 명시적으로 설정
         },
         rollupOptions: {
             external: ['react', 'react-dom'], // peerDependencies 제외
